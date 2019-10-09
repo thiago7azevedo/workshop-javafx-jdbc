3ae7c8bfa01afb635f8631fa4ecee333a140bfe6
Thiago Azevedo <thiago7azevedo@gmail.com>
2019-10-09 13:31:06 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -36,7 +40,19 @@ public class SellerFormController implements Initializable{
 	@FXML
 	private TextField txtName;
 	@FXML
+	private TextField txtEmail;
+	@FXML
+	private DatePicker dpBirthDate;
+	@FXML
+	private TextField txtBaseSalary;	
+	@FXML
 	private Label labelErrorName;
+	@FXML
+	private Label labelErrorEmail;
+	@FXML
+	private Label labelErrorBirhtDate;
+	@FXML
+	private Label labelErrorBaseSalary;
 	@FXML
 	private Button btSave;
 	@FXML
@@ -113,7 +129,10 @@ public class SellerFormController implements Initializable{
 	
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
-		Constraints.setTextFieldMaxLength(txtName, 30);
+		Constraints.setTextFieldMaxLength(txtName, 70);
+		Constraints.setTextFieldDouble(txtBaseSalary);
+		Constraints.setTextFieldMaxLength(txtEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 	
 	public void updateFormData() {
@@ -122,6 +141,14 @@ public class SellerFormController implements Initializable{
 		}
 		txtId.setText(String.valueOf(entity.getId())); // seta no campo ID o ID vindo do Seller, método criado e instanciado no começo da classe com entity
 		txtName.setText(entity.getName());//seta no campo name o valor buscado em entity de Seller
+		txtEmail.setText(entity.getEmail());// campod e texto, seta o update como email String
+		Locale.setDefault(Locale.US); // garantir que a formatação de baixo fique com .
+		txtBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));//converte para String o salario em double
+		if(entity.getBirthDate() != null) { // para não dar erro na conversão, verifica se não é nulo a getBirthDate
+		dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault())); //o BD usa LocalDate
+		//por isso precisa colocar o getBirthDate dentro de uma formatação especifica desta classe LocalDate, que usa
+		//ZoneId.systemDefault() para setar a data local da maquina do cliente
+		}
 	}
 
 	private void setErrorMessages(Map<String, String> errors) {
